7b8bdb87ee434aaf0c2f81d7ef74f60716c8d333
SandipCodes <sandipsh321@gmail.com>
2019-08-06 12:24:42 +0530

    Adding Class LoadObjectTest2

---
+++ Hibernate Proj1/src/org/ei/test/LoadObjectTest2.java
@@ -0,0 +1,11 @@
+package org.ei.test;
+
+public class LoadObjectTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Just Learnig GitHub");
+	}
+
+}
